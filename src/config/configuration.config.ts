56d081cac72cb6d528e76ae4e6b8aa0908e960d2
homayoon.gg <homysizerm@gmail.com>
2022-05-19 14:05:56 +0430

    config module and .env added

---
+++ src/config/configuration.config.ts
@@ -0,0 +1,4 @@
+export default () => ({
+    httpHost: 'localhost',
+    httpPort: process.env.HTTP_PORT || 3000,
+});
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ConfigService} from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+
+    //region CORS config
+
+    // If you do not want to block REST tools or server-to-server requests, add a !origin check in the origin function
+    const whitelist = [
+        'http://anotherhost.example',
+    ];
+    app.enableCors({
+        origin: function (origin, callback) {
+            if (whitelist.indexOf(origin) !== -1 || !origin) {
+                callback(null, true)
+            } else {
+                callback(new Error('Not allowed by CORS'))
+            }
+        },
+        credentials: true,
+    });
+
+    //endregion
+
+
+    await app.listen(configService.get<number>('httpPort'));
+
 }
+
 bootstrap();
